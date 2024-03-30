1d2e5504b24fde22a23a6e4b4d1ddca3d744d98d
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-03-30 21:35:37 +0530

    changed the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "IdeaClinic SRMIST",
+  description: "The Ultimate Discussion Forum for SRMIST faculty and students",
 };
 
 export default function RootLayout({
