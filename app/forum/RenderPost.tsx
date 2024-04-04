e04aa588f2955539e71078212e65cf3513abd041
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-04-04 15:13:29 +0530

    tooltip to the posts

--- app/forum/RenderPost.tsx
+++ app/forum/RenderPost.tsx
@@ -1,8 +1,13 @@
 'use client'
-import { Avatar } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { useToast } from '@/components/ui/use-toast';
-import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { CircleUserRound, Heart, Loader2, Share2 } from 'lucide-react';
 import Link from 'next/link';
@@ -17,6 +22,7 @@ interface Post {
     created_at: string;
     profiles: {
         avatar_url: string;
+        username: string;
     }
 }
 
@@ -34,7 +40,7 @@ const RenderPost = () => {
         try {
             let { data, error } = await supabase
             .from('posts')
-            .select('id, creator_id, title, likes, created_at, profiles( avatar_url )')
+            .select('id, creator_id, title, likes, created_at, profiles( avatar_url, username )')
             .range(0, 9)
             .order('created_at', { ascending: false })
             
@@ -136,12 +142,21 @@ const RenderPost = () => {
             {posts.map((post) => (
                 <div key={post.id}  className="bg-[#090909] w-full flex flex-row p-4 rounded-md shadow-md justify-between items-center">
                     <span className='flex flex-row w-1/3 justify-start gap-6 items-center'>
-                        <Avatar className='z-[100]'>
-                            <Link href={`/account/${post.creator_id}`}>
-                                <AvatarImage src={post.profiles.avatar_url} className=" object-cover object-center" />
-                                <AvatarFallback><CircleUserRound /></AvatarFallback>
-                            </Link>
-                        </Avatar>
+                        <TooltipProvider>
+                            <Tooltip>
+                                <TooltipTrigger>
+                                    <Avatar className='z-[100]'>
+                                        <Link href={`/account/${post.creator_id}`}>
+                                            <AvatarImage src={post.profiles.avatar_url} className=" object-cover object-center" />
+                                            <AvatarFallback><CircleUserRound /></AvatarFallback>
+                                        </Link>
+                                    </Avatar>
+                                </TooltipTrigger>
+                                <TooltipContent  className='bg-[#090909] text-white'>
+                                    {post.profiles.username}
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
                         <Link href={`/forum/post/${post.id}`}>
                             <h3 className="text-xl font-semibold text-white">{post.title}</h3>
                             <p className="text-gray-400">{new Date(post.created_at).toLocaleDateString()}</p>
