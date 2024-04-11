d0bdfd48511dccedd43ec11edafc9e301c7f542e
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-04-11 19:26:12 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -6,67 +6,67 @@ import InfiniteMovingTestimonials from "@/components/InfiniteMovingTestimonials"
 const ProductList = [
   {
     'title': "Product 1",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1707430393809-784967fe6fee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 2",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1698500278205-bf650c97a1f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 3",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1695376425475-1b6b561f8e4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 4",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1707430393809-784967fe6fee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 5",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1698500278205-bf650c97a1f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 6",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1695376425475-1b6b561f8e4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 7",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1699462515808-41f81a8145b0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 8",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1685094488656-9231107be07f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 9",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1677158607710-3731af5a8c31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 10",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1699462515808-41f81a8145b0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 11",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1685094488656-9231107be07f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 12",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1677158607710-3731af5a8c31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     'title': "Product 13",
-    'link': "#",
+    'link': "/forum",
     'thumbnail': "https://images.unsplash.com/photo-1699462515808-41f81a8145b0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   }
 ];
